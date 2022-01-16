9acb4a5f3ad2dcc15252254f8ee28545f623b53d
Lwmay <may.laurent@gmail.com>
2022-01-16 08:23:20 +0400

    Prepare tests Bach Report

--- batchReport/tests.py
+++ batchReport/tests.py
@@ -1,3 +1,21 @@
+import time
 from django.test import TestCase
+from batchReport.core.batchReport import BatchReport
 
-# Create your tests here.
+from batchReport.models import BatchModel
+
+
+class DatabaseTestCase(TestCase):
+    
+    def test_create_job_log(self):
+        
+        nbr_log_entries_before_add = BatchModel.objects.count()
+
+        new_entry = BatchReport()
+        new_entry.start('Test_job')
+        time.sleep(1)
+        new_entry.finish()
+
+        nbr_log_entries_after_add = BatchModel.objects.count()
+
+        self.assertTrue(nbr_log_entries_after_add == nbr_log_entries_before_add +1)
